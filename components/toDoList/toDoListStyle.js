e9bebf2163fd28f8bda00a5fd8dc88c22995b9b7
Julien <julien.bertocchini@gmail.com>
2019-01-18 11:16:18 +0100

    create toDolist component + style

---
+++ components/toDoList/toDoListStyle.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from "react-native";
+
+module.exports = {
+    toDoStyle : StyleSheet.create({
+        container: {
+            flex: 1,
+            backgroundColor: 'red',
+            alignItems: 'center',
+            justifyContent: 'center',
+
+        },
+        border: {
+            padding: 10,
+        }
+    })
+}
